8aef698e05f0040332fcbc750713f288aca08abb
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-01-06 17:32:33 +0900

    [add]sign_up page雛形追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :hobbyspot do
     get "/",      to: "home#index"
-    get "users",  to: "users#index"
+    get "users",  to: "users#sign_up"
   end
 end
