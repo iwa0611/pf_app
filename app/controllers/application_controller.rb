25f05680485613c00857605e3c46d417b2bfc5e2
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-01-09 10:41:04 +0900

    [add]users modelにnameカラム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  def after_sign_up_path_for(resource)
+    hobbyspot_users_edit_url
+  end
+
   def after_sign_in_path_for(resource)
     hobbyspot_url
   end
