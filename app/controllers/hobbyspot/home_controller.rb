0a12af919696c10be7cdd71ddde88e95e842bc1b
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-01-11 10:41:08 +0900

    [update]ユーザーメニュー追加・TOPに投稿表示される様に変更

--- app/controllers/hobbyspot/home_controller.rb
+++ app/controllers/hobbyspot/home_controller.rb
@@ -1,4 +1,8 @@
 class Hobbyspot::HomeController < ApplicationController
   def index
+    @posts = Post.all
+    if hobbyspot_user_signed_in?
+      @user = User.find_by(id: current_hobbyspot_user)
+    end
   end
 end
