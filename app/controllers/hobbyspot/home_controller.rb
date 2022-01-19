9e9d9d5b59b919491b4faa1aa51a29578311a727
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-01-19 10:51:18 +0900

    [update]クエリN1回避の為に追記

--- app/controllers/hobbyspot/home_controller.rb
+++ app/controllers/hobbyspot/home_controller.rb
@@ -1,6 +1,6 @@
 class Hobbyspot::HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.with_rich_text_body_and_embeds
     if hobbyspot_user_signed_in?
       @user = User.find_by(id: current_hobbyspot_user)
     end
