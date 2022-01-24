a3dc83d36619ce37b31fd41043ef23dfd5775a33
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-01-24 17:16:47 +0900

    [update]user sessionテスト修正

--- spec/requests/user_session_spec.rb
+++ spec/requests/user_session_spec.rb
@@ -1,23 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "SessionsController", type: :request do
+RSpec.describe "UserSessionsController", type: :request do
   describe "ログインしないでマイページを開いた時" do
     before do
       get hobbyspot_users_mypage_path
     end
 
     it "ログインページに移動する" do
-      expect(response).to redirect_to hobbyspot_user_session_path
-    end
-  end
-
-  describe "ログインしないで投稿ページを開いた時" do
-    before do
-      get new_hobbyspot_post_path
-    end
-
-    it "ログインページに移動する" do
-      expect(response).to redirect_to hobbyspot_user_session_path
+      expect(response).to redirect_to root_path
     end
   end
 
@@ -31,6 +21,17 @@ RSpec.describe "SessionsController", type: :request do
     it 'TOPページが表示される' do
       expect(controller.hobbyspot_user_signed_in?).to be true
       expect(response).to redirect_to hobbyspot_path
+    end
+
+    it 'ユーザー名が表示される' do
+      get hobbyspot_path
+      expect(response.body).to include @user.name
+    end
+
+    it 'ユーザーのメニューが表示される' do
+      get hobbyspot_path
+      expect(response.body).to include 'マイページ'
+      expect(response.body).to include 'ログアウト'
     end
   end
 end
