bf1c398b76904dd3c9399431660e24665a718e03
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-01-25 09:28:21 +0900

    [update]user_sessionテスト　項目追加

--- spec/requests/user_session_spec.rb
+++ spec/requests/user_session_spec.rb
@@ -18,20 +18,25 @@ RSpec.describe "UserSessionsController", type: :request do
       post hobbyspot_user_session_path
     end
 
-    it 'TOPページが表示される' do
+    it "TOPページが表示される" do
       expect(controller.hobbyspot_user_signed_in?).to be true
       expect(response).to redirect_to hobbyspot_path
     end
 
-    it 'ユーザー名が表示される' do
+    it "ユーザー名が表示される" do
       get hobbyspot_path
       expect(response.body).to include @user.name
     end
 
-    it 'ユーザーのメニューが表示される' do
+    it "ユーザーのメニューが表示される" do
       get hobbyspot_path
       expect(response.body).to include 'マイページ'
       expect(response.body).to include 'ログアウト'
+    end
+
+    it "マイページが表示される" do
+      get hobbyspot_users_mypage_path
+      expect(response).to have_http_status 200
     end
   end
 end
