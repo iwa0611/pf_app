9247d2adc81d417aad4dc20442921f66eaaf1846
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-01-25 10:03:59 +0900

    [add]spec/model postテスト追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  describe "投稿する時" do
+    let(:post) { create(:post) }
+
+    subject { post.valid? }
+
+    it "bodyが空だと投稿できない" do
+      post.body = nil
+      is_expected.to eq false
+    end
+  end
 end
