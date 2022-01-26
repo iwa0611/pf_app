5938eef845a084484871e6bdd6b7d709c85d16d1
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-01-26 09:54:20 +0900

    [add]spec/controller post_spec追加

---
+++ spec/controllers/post_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Hobbyspot::PostsController do
+
+  before do
+    @admin = create(:admin)
+    sign_in @admin
+  end
+  
+  describe "POST #create" do
+    it "adminで投稿ができる" do
+      expect{
+        post :create, params: { body: "test" }
+      }.to change(Post, :count).by(1)
+    end
+  end
+
+  describe "GET #edit #update #delete" do
+    let(:post) { create(:post, body: "test") }
+
+    it "adminでeditが開ける" do
+      get :edit, params: {id: post.id}
+      expect(response).to have_http_status 200
+    end
+
+    it "adminで投稿をupdateできる" do
+      patch :update, params: {post: attributes_for(:post, body: "test1"), id: post.id}
+      expect(post.reload.body.to_s).to include "test1"
+    end
+
+    it "adminで投稿をdeleteできる" do
+      post.body   #呼び出さないとdestroyが実行されない
+      expect {
+        delete :destroy, params: {id: post.id}
+      }.to change(Post, :count).by(-1)
+    end
+  end
+end
